82abde2ab71862868c5face6d0ffd655c310e072
Havu Miikonen <havu.mik@gmail.com>
2019-02-26 10:48:27 +0200

    created file

---
+++ gcd-complexity/gcd.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def badGCD(n, m):
+    """
+    A naive and unintuitive algorithm that calculates the greatest common
+    divisor of two numbers. 
+    Arguments: non-negative integers n and m
+    Returns: gcd(n, m) and the number of iterations (~ nmber of subtractions
+    and comparisons) needed.
+    """
+    rounds = 0
+    while n > 0 and m > 0:
+        if n > m:
+            n -= m
+        else:
+            m -= n
+        rounds += 1
+    return((n, rounds))
+
+
+#def GCD(n, m):
+#    """
+#    The classic Euclidean algorithm implemented recursively
+#    Arguments: non-negative integers n and m
+#    Returns: the gcd of n and m and the number of iterations needed
+#    """ 
